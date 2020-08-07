4ba656dc978d52212f063ccb77c45b11adf15b42
WashingtonKK <washingtonkigan@gmail.com>
2020-08-07 16:01:46 +0300

    Setting up

---
+++ src/justchat/justchat/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from chat.views import index
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+]
